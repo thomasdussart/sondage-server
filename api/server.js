26e3e5f7c062bc823c88981bd4e44599345b52dd
Thomas Dussart <thomas.dussart@gmail.com>
2024-09-20 14:07:09 +0200

    vercel

--- api/server.js
+++ api/server.js
@@ -5,23 +5,19 @@ const verifyToken = require("./middleware/authMiddleware");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const cors = require("cors");
-const corsOptions = {
-  origin: "https://sondage-sand.vercel.app",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+
+app.use(cors());
 
 require("../middleware/allowCors");
 
 // routes categories
-require("./routes/categories", verifyToken)(app);
+require("../routes/categories", verifyToken)(app);
 // routes login
-require("./routes/login", verifyToken)(app);
+require("../routes/login", verifyToken)(app);
 // routes questions
-require("./routes/questions", verifyToken)(app);
+require("../routes/questions", verifyToken)(app);
 // routes users
-require("./routes/users", verifyToken)(app);
+require("../routes/users", verifyToken)(app);
 
 // app.listen(port, () => {
 //   console.log(`Server listening at http://localhost:${port}`);
